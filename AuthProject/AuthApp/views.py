542f2173791052b686574a55b5bcfebf36d4ec63
moolasudhakarreddy31 <moolasudhakarreddy8008@gamil.com>
2023-04-13 19:38:21 +0530

    Some code is Excutited

--- AuthProject/AuthApp/views.py
+++ AuthProject/AuthApp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def home_view(request):
+
+    return render(request,'AuthApp/home.html')
+
+
+
+@login_required
+def java_view(request):
+
+    return render(request,'AuthApp/java.html')
+
+
+def python_view(request):
+
+    return render(request,'AuthApp/python.html')
+
+
+def aptitude_view(request):
+
+    return render(request,'AuthApp/aptitude.html')
+
+
+
+
+
--- AuthProject/AuthProject/urls.py
+++ AuthProject/AuthProject/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from AuthApp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('java/',views.java_view),
+    path('python/',views.python_view),
+    path('aptitude/',views.aptitude_view),
+    path('home/',views.home_view),
+    path('accounts/',include('django.contrib.auth.urls')),
+
 ]
